972128ca68c39080592e540303255be1edcfc566
iRobo <25803834+iRobo1@users.noreply.github.com>
2026-03-11 03:42:05 +0100

    Update AuroraAlertBot.py
    Added license

--- AuroraAlertBot.py
+++ AuroraAlertBot.py
@@ -1,3 +1,19 @@
+# Aurora Alert Telegram Bot
+# Copyright (C) 2026 Sandro Karhula
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -398,4 +414,5 @@ if __name__ == '__main__':
     loadUserPreferences()
 
     print('Polling...')
+
     app.run_polling(poll_interval=3)
